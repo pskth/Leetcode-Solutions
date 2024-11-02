f3d9a04cdd3df1dad4bc639d1f5fd6a50824625e
Parimi Saketh Kumar <154651191+pskth@users.noreply.github.com>
2024-11-02 19:37:24 +0530

    Update circular-sentence.py

--- circular-sentence.py
+++ circular-sentence.py
@@ -11,3 +11,17 @@ class Solution:
                 return False
             fl = word[-1]
         return True
+        '''
+        class Solution:
+            def isCircularSentence(self, sentence: str) -> bool:
+                words:list(int) = sentence.split(" ")
+                #Intially we use fl to compare first letter of first word with last letter of the last word. Since in the first iteration we don't have to do any other comparisions, we use fl to store the last letter of the last word.
+                fl = words[-1][-1]
+                ll = words[-1][-1]
+                for word in words:
+                    ll = word[0]
+                    if fl != ll:
+                        return False
+                    fl = word[-1]
+                return True
+        '''
