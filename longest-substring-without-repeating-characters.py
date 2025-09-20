e0e5d00128db320baceeeae5d172f4f1ea7e471c
Parimi Saketh Kumar <154651191+pskth@users.noreply.github.com>
2025-09-20 20:38:18 +0530

    longest-substring-without-repeating-characters.py | Committed via CodePipe.

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        longest = 0
+        l = 0
+        for r in range(len(s)):
+            while l < r and s[r] in seen:
+                seen.discard(s[l])
+                l += 1
+
+            seen.add(s[r])
+            longest = max(longest, r - l + 1)
+            
+
+        return longest
+            
