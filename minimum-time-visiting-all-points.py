f7078ae62e4f203af1385a3aa4570124928f711b
Parimi Saketh Kumar <154651191+pskth@users.noreply.github.com>
2026-01-12 20:17:47 +0530

    Add minimum-time-visiting-all-points.py

---
+++ minimum-time-visiting-all-points.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        res = 0
+
+        for i in range(1, len(points)):
+            x1, y1 = points[i - 1]
+            x2, y2 = points[i]
+            
+            dx = abs(x2 -x1)
+            dy = abs(y2-  y1)
+            
+            res += dx + dy - min(dx, dy)
+
+        return res
