8706ed108f5d1ea47d169051532ddbdca5b12490
Parimi Saketh Kumar <154651191+pskth@users.noreply.github.com>
2026-06-05 09:15:09 +0530

    Add largestRectangleArea method in Solution class
    Implement largest rectangle area calculation using a monotonic stack.

---
+++ largest-rectangle-in-histogram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        """
+        monotonic increasing stack
+
+        """
+        stack = []
+        res = 0
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                idx, hei = stack.pop()
+                res = max(res, hei * (i - idx))
+                start = idx
+
+            stack.append((start, h))
+
+        for i, h in stack:
+            res = max(res, h * (len(heights) - i))
+        
+        return res
